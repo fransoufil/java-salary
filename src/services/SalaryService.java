fe1cab8ff3b1fb6a3678b467dc8786db5854008b
fransoufil <fran.sf@uol.com.br>
2023-11-17 13:47:53 -0300

    SalaryService e Employee

--- src/services/SalaryService.java
+++ src/services/SalaryService.java
@@ -1,5 +1,15 @@
 package services;
 
+import entities.Employee;
+
 public class SalaryService {
+	
+	PensionService pensionService = new PensionService();
+	TaxService taxService = new TaxService();
+	
+	public double netSalary(Employee employee) {
+		return employee.getGrossSalary() - taxService.tax(employee.getGrossSalary()
+				- pensionService.pension(employee.getGrossSalary()));
+	}
 
 }
