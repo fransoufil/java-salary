ef3fe4110956eddf4229b2be575ec27e824477fb
fransoufil <fran.sf@uol.com.br>
2023-11-17 12:06:14 -0300

    Pension e Tax Service

--- src/services/PensionService.java
+++ src/services/PensionService.java
@@ -1,5 +1,9 @@
 package services;
 
 public class PensionService {
+	
+	public double pension(double amount) {
+		return amount * 0.7;
+	}
 
 }
--- src/services/TaxService.java
+++ src/services/TaxService.java
@@ -1,5 +1,9 @@
 package services;
 
 public class TaxService {
+	
+	public double tax(double amount) {
+		return amount * 0.2;
+	}
 
 }
