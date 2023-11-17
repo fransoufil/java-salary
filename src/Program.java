a492eb350c99c9e34b6332bce1507618d5f33d3e
fransoufil <fran.sf@uol.com.br>
2023-11-17 13:54:39 -0300

    netSalary em main Program

--- src/Program.java
+++ src/Program.java
@@ -1,6 +1,9 @@
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Employee;
+import services.SalaryService;
+
 public class Program {
 
 	public static void main(String[] args) {
@@ -12,6 +15,14 @@ public class Program {
 		
 		System.out.println("Salario bruto do funcionario: ");
 		double grossSalary = sc.nextDouble();
+		
+		Employee employee = new Employee(name, grossSalary);
+		
+		SalaryService salaryService = new SalaryService();
+		
+		double netSalary = salaryService.netSalary(employee);
+		
+		System.out.printf("Salario liquido do funcionario = %.2f%n", netSalary);
 		
 		sc.close();
 	}
